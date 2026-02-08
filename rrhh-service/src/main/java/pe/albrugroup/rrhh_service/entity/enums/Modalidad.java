d905cab08a1b5906d8388ccd55688c9640e9389f
Bizzard <jevbxx@gmail.com>
2026-02-08 05:40:06 -0500

    Se agregaron los enums: Modalidad, SeguroSalud, SistemaPensiones

---
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/entity/enums/Modalidad.java
@@ -0,0 +1,8 @@
+package pe.albrugroup.rrhh_service.entity.enums;
+
+public enum Modalidad {
+    PART_TIME,
+    FULL_TIME,
+    SEMI_FULL,
+    SUPER_FULL
+}
---
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/entity/enums/SistemaPensiones.java
@@ -0,0 +1,9 @@
+package pe.albrugroup.rrhh_service.entity.enums;
+
+public enum SistemaPensiones {
+    ONP,
+    AFP_INTEGRA,
+    PROFUTURO_AFP,
+    AFP_HABITAD,
+    PRIMA_AFP
+}
