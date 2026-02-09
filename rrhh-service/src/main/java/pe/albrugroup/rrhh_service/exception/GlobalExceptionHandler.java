f1d255ec6ec7ed6b66368ddf4f5aac48bd4dd7df
Bizzard <jevbxx@gmail.com>
2026-02-09 14:43:33 -0500

    Update: Exceptions+

--- rrhh-service/src/main/java/pe/albrugroup/rrhh_service/exception/GlobalExceptionHandler.java
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/exception/GlobalExceptionHandler.java
@@ -14,29 +14,53 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // EXCEPTIONS PERSONALIZADAS
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Map<String,Object>> handleBusinessException(BusinessException e) {
+        Map<String,Object> body = new LinkedHashMap<>();
 
+        body.put("status", e.getStatus().value());
+        body.put("error", e.getStatus().getReasonPhrase());
+        body.put("message", e.getMessage());
+        if(e.getId() != null) body.put("id", e.getId());
+        if(e.getDetails() != null) body.put("details", e.getDetails());
+
+        return ResponseEntity.status(e.getStatus()).body(body);
+    }
+    // CAMPOS INVALIDOS EN REQUESTS
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationErrors(MethodArgumentNotValidException e) {
         Map<String, Object> errors = new LinkedHashMap<>();
-        errors.put("status", HttpStatus.BAD_REQUEST.value());
-        errors.put("error", "Bad Request");
-
         List<String> message = e.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
-        errors.put("message", message);
+
+        errors.put("status", HttpStatus.BAD_REQUEST.value());
+        errors.put("error", "Bad Request");
+        errors.put("message", "Campos Invalidos en la solicitud");
+        errors.put("details", message);
 
         return ResponseEntity.badRequest().body(errors);
     }
 
+    // PARAMETROS INVALIDOS EN LOS ENDPOINTS
     @ExceptionHandler({MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
     public ResponseEntity<?> handleBadRequest(Exception e) {
         return ResponseEntity.badRequest().body(Map.of(
                 "status", 400,
                 "error", "Bad Request",
-                "mensaje", "Parámetros inválidos"
+                "message", "Parámetros inválidos"
+        ));
+    }
+    // CATCH-ALL - Exceptions No Mapeados
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleUnexpected(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+                "status", 500,
+                "error", "Internal Server Error",
+                "message", "Ocurrió un error inesperado"
         ));
     }
 }
