45f289f75a542143cc17439cac1ab5884f9ee621
Bizzard <jevbxx@gmail.com>
2026-02-18 15:19:17 -0500

    Fix: Corrigiendo el PasswordEncoder

--- auth-service/src/main/java/pe/albrugroup/auth_service/configuration/PasswordEncoderConfig.java
+++ auth-service/src/main/java/pe/albrugroup/auth_service/configuration/PasswordEncoderConfig.java
@@ -3,12 +3,13 @@ package pe.albrugroup.auth_service.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class PasswordEncoderConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- auth-service/src/main/java/pe/albrugroup/auth_service/configuration/SecurityConfig.java
+++ auth-service/src/main/java/pe/albrugroup/auth_service/configuration/SecurityConfig.java
@@ -1,5 +1,20 @@
 package pe.albrugroup.auth_service.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import pe.albrugroup.auth_service.security.AuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationFilter authFilter;
+    private final UserDetailsService userDetailsService;
+
+
 }
