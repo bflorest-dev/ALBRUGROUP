d7450c87c87207d4c5ab7f1dfef65164893f295b
Bizzard <jevbxx@gmail.com>
2026-02-19 14:27:17 -0500

    Add: Properties

--- auth-service/src/main/java/pe/albrugroup/auth_service/security/JWTUtil.java
+++ auth-service/src/main/java/pe/albrugroup/auth_service/security/JWTUtil.java
@@ -4,13 +4,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
