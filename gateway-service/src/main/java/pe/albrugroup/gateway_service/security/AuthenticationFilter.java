e3860a703b98210b9a195519c9ce5a4d311500ca
Bizzard <jevbxx@gmail.com>
2026-02-19 14:39:00 -0500

    Add: Gateway Authentication Filter

--- gateway-service/src/main/java/pe/albrugroup/gateway_service/security/AuthenticationFilter.java
+++ gateway-service/src/main/java/pe/albrugroup/gateway_service/security/AuthenticationFilter.java
@@ -1,4 +1,72 @@
 package pe.albrugroup.gateway_service.security;
 
-public class AuthenticationFilter {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component @Slf4j
+@RequiredArgsConstructor
+public class AuthenticationFilter implements   WebFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return chain.filter(exchange);
+        }
+        String token = authHeader.substring(7);
+        try {
+            if (!jwtUtil.validateToken(token)) {
+                log.warn("Token inválido o expirado");
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String username = jwtUtil.extractUsername(token);
+            List<String> roles = jwtUtil.extractRoles(token);
+            List<String> permisos = jwtUtil.extractPermisos(token);
+
+            log.debug("JWT válido - Usuario: {}, Roles: {}", username, roles);
+
+            List<SimpleGrantedAuthority> authorities = Stream.concat(
+                    roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role)),
+                    permisos.stream()
+                            .map(SimpleGrantedAuthority::new)
+            ).collect(Collectors.toList());
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            authorities
+                    );
+
+            return chain.filter(exchange)
+                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+
+        } catch (Exception e) {
+            log.error("Error validando JWT: {}", e.getMessage());
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
 }
--- gateway-service/src/main/java/pe/albrugroup/gateway_service/security/JwtUtil.java
+++ gateway-service/src/main/java/pe/albrugroup/gateway_service/security/JwtUtil.java
@@ -1,4 +1,59 @@
 package pe.albrugroup.gateway_service.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public boolean validateToken(String token) {
+        try {
+            extractAllClaims(token);
+            return !isTokenExpired(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+    public Long extractEmployeeId(String token) {
+        return extractClaim(token, claims -> claims.get("employeeId", Long.class));
+    }
+    public List<String> extractRoles(String token) {
+        return extractClaim(token, claims -> claims.get("roles", List.class));
+    }
+    public List<String> extractPermisos(String token) {
+        return extractClaim(token, claims -> claims.get("permisos", List.class));
+    }
+    public Claims extractAllClaims(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 }
