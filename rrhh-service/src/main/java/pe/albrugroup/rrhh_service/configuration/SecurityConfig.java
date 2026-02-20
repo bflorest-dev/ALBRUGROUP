6e77f8725f192755a07a016e04ad91db4ec6ff9f
Bizzard <jevbxx@gmail.com>
2026-02-20 18:36:51 -0500

    Add: SecurityFilterChain en el rrhh-service

--- rrhh-service/src/main/java/pe/albrugroup/rrhh_service/configuration/SecurityConfig.java
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/configuration/SecurityConfig.java
@@ -1,23 +1,36 @@
 package pe.albrugroup.rrhh_service.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pe.albrugroup.rrhh_service.security.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationFilter authFilter;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
--- rrhh-service/src/main/java/pe/albrugroup/rrhh_service/security/AuthenticationFilter.java
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/security/AuthenticationFilter.java
@@ -1,4 +1,63 @@
 package pe.albrugroup.rrhh_service.security;
 
-public class AuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component @Slf4j
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
+        final String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = authHeader.substring(7);
+
+        try {
+            String username = jwtUtil.extractUsername(token);
+            List<String> roles = jwtUtil.extractRoles(token);
+            List<String> permisos = jwtUtil.extractPermisos(token);
+            log.debug("JWT recibido - Usuario: {}, Roles: {}", username, roles);
+
+            List<SimpleGrantedAuthority> authorities = Stream.concat(
+                    roles.stream()
+                            .map(rol -> new SimpleGrantedAuthority("ROLE_" + rol)),
+                    permisos.stream()
+                            .map(SimpleGrantedAuthority::new)
+            ).toList();
+
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(username, null, authorities);
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        } catch (Exception e) {
+            log.error("Error leyendo JWT: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
