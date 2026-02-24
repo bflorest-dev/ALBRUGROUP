02cdb3502a394ffcfc3073345652eb4e78aad179
Bizzard <jevbxx@gmail.com>
2026-02-24 11:29:32 -0500

    Fix: SubTipificion en Reclutamiento

--- rrhh-service/src/main/java/pe/albrugroup/rrhh_service/entity/enums/ReclutamientoSubEstado.java
+++ rrhh-service/src/main/java/pe/albrugroup/rrhh_service/entity/enums/ReclutamientoSubEstado.java
@@ -3,6 +3,7 @@ package pe.albrugroup.rrhh_service.entity.enums;
 public enum ReclutamientoSubEstado {
     NO_DESEA_PUESTO,
     POCA_FLUIDEZ_VERBAL,
+    SIN_HABILIDADES_COMERCIALES,
     INEXPERIENCIA,
     PROBLEMAS_CON_HORARIOS,
     DISTANCIA_TIEMPO,
