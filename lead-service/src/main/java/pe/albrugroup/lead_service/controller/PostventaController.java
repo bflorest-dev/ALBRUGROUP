5f83290616baf7f06836257badacee66556529b6
Bizzard <jevbxx@gmail.com>
2026-04-28 14:41:14 -0500

    Add: Separacion coherente de endpoints que gestionan el Lead por etapas

---
+++ lead-service/src/main/java/pe/albrugroup/lead_service/controller/PostventaController.java
@@ -0,0 +1,4 @@
+package pe.albrugroup.lead_service.controller;
+
+public class PostventaController {
+}
---
+++ lead-service/src/main/java/pe/albrugroup/lead_service/controller/PreventaController.java
@@ -0,0 +1,17 @@
+package pe.albrugroup.lead_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.albrugroup.lead_service.service.LeadService;
+
+@RestController @Validated
+@RequiredArgsConstructor
+@RequestMapping("/preventa")
+public class PreventaController {
+
+    private final LeadService  leadService;
+
+
+}
