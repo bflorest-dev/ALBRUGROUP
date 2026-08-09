63d1456067dee12beb80bf98bf9e4e488bd5f036
Bizzard <jevbxx@gmail.com>
2026-08-09 15:26:33 -0500

    Update: Responsividad en ASESOR_VENTA 5

--- frontend/src/app/features/asesor-ventas/components/lead-management-dialog/lead-management-dialog.component.ts
+++ frontend/src/app/features/asesor-ventas/components/lead-management-dialog/lead-management-dialog.component.ts
@@ -3,11 +3,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
 import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 import { SelectModule } from 'primeng/select';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TagModule } from 'primeng/tag';
-import { TextareaModule } from 'primeng/textarea';
 import { LeadCommercialDataTabsComponent } from '../../../../shared/components/lead-commercial-data-tabs/lead-commercial-data-tabs.component';
 import { PhoneActionButtonComponent } from '../../../../shared/components/phone-action-button/phone-action-button.component';
 import { PhoneNumberFieldComponent } from '../../../../shared/components/phone-number-field/phone-number-field.component';
@@ -20,11 +20,11 @@ import { AsesorVentasWorkspaceFacade } from '../../facades/asesor-ventas-workspa
     ButtonModule,
     DatePickerModule,
     DialogModule,
+    InputTextModule,
     MessageModule,
     SelectModule,
     SkeletonModule,
     TagModule,
-    TextareaModule,
     PhoneActionButtonComponent,
     PhoneNumberFieldComponent,
     LeadCommercialDataTabsComponent
