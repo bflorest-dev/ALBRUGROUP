ac3d592b55012106097aa4a5eaf726ba3cba6563
Bizzard <jevbxx@gmail.com>
2026-09-11 13:28:19 -0500

    PREVENTA: Drawer corregido 2

--- lead-service/src/main/java/pe/albrugroup/lead_service/entity/request/LeadDireccionRequest.java
+++ lead-service/src/main/java/pe/albrugroup/lead_service/entity/request/LeadDireccionRequest.java
@@ -18,16 +18,13 @@ public class LeadDireccionRequest {
     private TipoVia tipoVia;
     private String via;
 
-    @NotBlank(message = "direccion es obligatoria")
     private String direccion;
     private String referencia;
 
-    @NotBlank(message = "latitud es obligatoria")
     @Size(max = 64, message = "latitud no debe superar 64 caracteres")
     @Pattern(regexp = "-?\\d{1,3}([\\.,]\\d+)?", message = "latitud no tiene un formato valido")
     private String latitud;
 
-    @NotBlank(message = "longitud es obligatoria")
     @Size(max = 64, message = "longitud no debe superar 64 caracteres")
     @Pattern(regexp = "-?\\d{1,3}([\\.,]\\d+)?", message = "longitud no tiene un formato valido")
     private String longitud;
